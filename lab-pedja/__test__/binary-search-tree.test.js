d4390002f491d39357d71f6f29493edca03bc50b
Predrag Josifovic <pjosifovic@yahoo.com>
2017-12-18 18:10:36 -0800

    passing TESTS for FIND

--- lab-pedja/__test__/binary-search-tree.test.js
+++ lab-pedja/__test__/binary-search-tree.test.js
@@ -9,6 +9,7 @@ bst.insert(8);
 bst.insert(16);
 
 describe('binary-search-tree.js', () => {
+  // INSERT testing
   test('INSERT method should insert new node into BST', () => {
     expect(bst.right.value).toEqual(16);
     expect(bst.left.value).toEqual(5);
@@ -20,4 +21,15 @@ describe('binary-search-tree.js', () => {
   test('INSERT method should return an error is passed in value is not a number', () => {
     expect(()=> {bst.insert('testing');}).toThrow();
   });
+
+  // FIND testing
+  test('FIND method should return value of a node if bst contains that node value', () => {
+    expect(bst.find(8).value).toEqual(8);
+    expect(bst.find(16).value).toEqual(16);    
+  });
+  test('FIND method should return NULL if value doesn\'t exist', () => {
+    expect(bst.find(100)).toEqual(null);
+  });
+
+
 });
